9ed7a7338e8ad8054704b982929f1eda0d5675ae
Elvis <elviskb@gmail.com>
2022-07-31 17:27:38 +0300

    create and delete ready

---
+++ src/components/Notes/Note.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { computed } from '@vue/reactivity';
+
+
+  const props = defineProps({
+    note: {
+      type: Object,
+      default: null
+    }
+  })
+
+  /*
+    Define emits
+  */
+ const emits = defineEmits(['deleteClicked'])
+
+  /*
+    Computed methods
+  */
+  const characterLength = computed(() => {
+    const length = props.note.content.length
+    const description = length  > 1 ? 'characters' : 'character'
+    return length + ' ' + description
+  })
+
+  /*
+    Methods
+  */
+  const handleDeleteClicked = () => {
+    emits('deleteClicked', props.note.id)
+  }
+</script>
+
+<template>
+  <div
+      class="card mb-4"
+    >
+      <header class="card-header">
+      </header>
+      <div class="card-content">
+        <div class="content">
+          {{ note.content }}
+          <div class="has-text-right has-text-grey-light mt-2">
+            <small>{{ characterLength }}</small>
+          </div>
+        </div>
+      </div>
+      <footer class="card-footer">
+        <!-- <a href="#" class="card-footer-item">Save</a> -->
+        <a href="#" class="card-footer-item">Edit</a>
+        <a
+          @click.prevent="handleDeleteClicked"
+          class="card-footer-item"
+          href="#"
+        >
+          Delete
+
+        </a>
+      </footer>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,42 +1,90 @@
+<script setup>
+import { ref } from 'vue';
+import Note from '../components/Notes/Note.vue';
+
+/*
+  Notes
+*/
+  const newNote = ref('')
+  const newNoteRef = ref(null)
+  const notes = ref([
+    {
+      id: 1,
+      content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque eum, nam ab velit culpa quibusdam excepturi expedita itaque accusamus eius corporis dignissimos. Dolore, libero? Sequi quod labore cupiditate quisquam natus.'
+    },
+    {
+      id: 2,
+      content: 'This is a shorter note. It is way shorter than the Lorem ispsum dolor sit.'
+    }
+  ])
+
+  /*
+    methods
+  */
+
+ const addNewNote = () => {
+
+  const currentDate = new Date().getTime(),
+    id = currentDate.toString()
+
+  // console.log(currentDate)
+
+  const myNote = {
+    id,
+    content: newNote.value,
+  }
+  // Add to the front of the array
+  notes.value.unshift(myNote)
+
+// Clear the form
+  newNote.value = ''
+
+  // Set focus back to the form after saving a note
+  newNoteRef.value.focus()
+ }
+
+ const deleteNote = (index) => {
+    notes.value = notes.value.filter((note) => { return note.id != index })
+    // console.log(index)
+ }
+
+</script>
+
 <template>
   <div class="notes">
     <div class="card has-background-danger-dark mb-5 p-4">
       <div class="field">
         <div class="control">
           <textarea
+            v-model="newNote"
             class="textarea"
             placeholder="Add a new note"
+            ref="newNoteRef"
           />
         </div>
       </div>
 
       <div class="field is-grouped is-grouped-right">
         <div class="control">
-          <button class="button is-link has-background-danger">Add New Note</button>
+          <button
+            @click="addNewNote(newNote)"
+            class="button is-link has-background-danger"
+            :disabled="!newNote"
+          >
+            Add New Note
+          </button>
         </div>
       </div>
     </div>
-    <div v-for="i in 3"  class="card mb-4">
-      <header class="card-header">
-      </header>
-      <div class="card-content">
-        <div class="content">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque eum, nam ab velit culpa quibusdam excepturi expedita itaque accusamus eius corporis dignissimos. Dolore, libero? Sequi quod labore cupiditate quisquam natus.
-        </div>
-      </div>
-      <footer class="card-footer">
-        <!-- <a href="#" class="card-footer-item">Save</a> -->
-        <a href="#" class="card-footer-item">Edit</a>
-        <a href="#" class="card-footer-item">Delete</a>
-      </footer>
-    </div>
+    <Note
+      v-for="note in notes"
+      :key="note.id"
+      :note="note"
+      @delete-clicked="deleteNote"
+    />
   </div>
 </template>
 
-<script setup>
-
-</script>
-
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
